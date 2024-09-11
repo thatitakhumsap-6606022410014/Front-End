0cb70940585abd679249c37bfd58d3c40bbd0a5a
thatitakhumsap-6606022410014 <138425547+thatitakhumsap-6606022410014@users.noreply.github.com>
2024-09-11 09:28:03 +0700

    1

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const axios = require('axios');
-const app = express();
-const path = require("path")
 var bodyParser = require('body-parser');
+const path = require("path")
+const app = express();
 
 // Base URL for the API
 const base_url = "http://localhost:3000"
 
-app.set("views",path.join(__dirname,"/views"));
+app.set("views",path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
